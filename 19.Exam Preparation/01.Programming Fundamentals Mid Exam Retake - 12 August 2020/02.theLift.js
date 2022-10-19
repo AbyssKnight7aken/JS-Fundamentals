d2bb01dbaf09e3d3386ff6722fdaabd0435212fc
AbyssKnight7aken <7akenbg@gmail.com>
2022-10-20 01:52:48 +0300

    New solved problem!

---
+++ 19.Exam Preparation/01.Programming Fundamentals Mid Exam Retake - 12 August 2020/02.theLift.js
@@ -0,0 +1,42 @@
+function theLift(inputArray) {
+
+    const wagonCapacity = 4;
+    let peopleWaiting = Number(inputArray.shift());
+    let wagons = inputArray[0].split(` `).map(Number);
+
+    for (let i = 0; i < wagons.length; i++) {
+        if (peopleWaiting <= 0) { break; }
+        let currentWagon = Number(wagons[i]);
+        if (currentWagon < wagonCapacity) {
+            peopleWaiting -= wagonCapacity - currentWagon;
+            if (peopleWaiting < 0) {
+                wagons[i] = wagonCapacity + peopleWaiting;
+                peopleWaiting = 0;
+            }
+            else {
+                wagons[i] = wagonCapacity;
+            }
+        }
+    }
+
+    let availability = wagons.filter(x => x < 4).reduce((a, b) => a + b, 0);
+
+    if (peopleWaiting > 0) {
+        console.log(`There isn't enough space! ${peopleWaiting} people in a queue!`);
+        console.log(wagons.join(` `));
+    }
+
+    if (availability > 0) {
+        console.log(`The lift has empty spots!`);
+        console.log(wagons.join(` `));
+    }
+
+    if (peopleWaiting <= 0 && availability <= 0) {
+        console.log(wagons.join(` `));
+    }
+
+}
+
+//theLift(["15", "0 0 0 0 0"])
+//theLift(["20", "0 2 0"])
+theLift(["4", "4 0 4"])
