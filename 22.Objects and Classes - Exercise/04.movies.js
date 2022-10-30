6aa10802324b0efaf474a9604bcd205bd3100526
Radoslav Petrov <115632451+AbyssKnight7aken@users.noreply.github.com>
2022-10-31 01:53:29 +0200

    New solved problem!

---
+++ 22.Objects and Classes - Exercise/04.movies.js
@@ -0,0 +1,50 @@
+function movies(inputData) {
+
+    const moviesInfo = [];
+
+    for (const line of inputData) {
+        if (line.includes(`addMovie`)) {
+            let name = line.split(`addMovie `)[1];
+            moviesInfo.push({ name });
+        }
+        else if (line.includes(`directedBy`)) {
+            let [name, director] = line.split(` directedBy `);
+            let movie = moviesInfo.find(el => el.name === name);
+            if (movie) {
+                movie.director = director;
+            }
+        }
+        else if (line.includes(`onDate`)) {
+            let [name, date] = line.split(` onDate `);
+            let movie = moviesInfo.find(el => el.name === name);
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    }
+
+    moviesInfo.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    })
+
+}
+
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'])
+
+movies([
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'])    
