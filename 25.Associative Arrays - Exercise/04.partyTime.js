12a14968dfb524f08fb38f65dc22f78c0b154edc
Radoslav Petrov <115632451+AbyssKnight7aken@users.noreply.github.com>
2022-11-05 19:25:16 +0200

    New solved problem!

---
+++ 25.Associative Arrays - Exercise/04.partyTime.js
@@ -0,0 +1,64 @@
+function partyTime(input) {
+
+    const vip = [];
+    const regular = [];
+    let person = input.shift();
+
+    while (person !== `PARTY`) {
+        if (!isNaN(person[0])) { vip.push(person) }
+        else { regular.push(person) }
+        person = input.shift();
+    }
+
+    const allGuest = vip.concat(regular);
+
+    for (const element of input) {
+        allGuest.splice(allGuest.indexOf(element), 1);
+    }
+
+    console.log(allGuest.length);
+    allGuest.forEach(guest => { console.log(guest) })
+
+}
+
+partyTime([
+    '7IK9Yo0h',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc',
+    'tSzE5t0p',
+    'PARTY',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc'])
+
+partyTime([
+    'm8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ',
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'xys2FYzn',
+    'MDzcM9ZK',
+    'PARTY',
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'm8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ'])
