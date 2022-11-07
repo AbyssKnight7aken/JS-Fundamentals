c3e73344a88bed7470df5ce2823763dab4521749
AbyssKnight7aken <7akenbg@gmail.com>
2022-11-08 00:26:23 +0200

    changes!

---
+++ 25.Associative Arrays - Exercise/more01.garage-judge.js
@@ -0,0 +1,33 @@
+function garage(inputData) {
+
+    let carsInfo = {};
+
+    for (const line of inputData) {
+        let = [garageNumber, cars] = [line.split(` - `)[0], line.split(` - `)[1]];
+        if (!carsInfo.hasOwnProperty(garageNumber)) {
+            carsInfo[garageNumber] = [cars];
+        } else {
+            carsInfo[garageNumber] = carsInfo[garageNumber].concat(cars);
+        }
+    }
+
+    for (const garage in carsInfo) {
+        console.log(`Garage № ${garage}`);
+        carsInfo[garage].forEach(car => {
+            let result = car.toString().replace(/[":"]/g, " -");
+            console.log(`--- ${result}`);
+        });
+    }
+}
+
+garage([
+    '1 - color: blue, fuel type: diesel',
+    '1 - color: red, manufacture: Audi',
+    '2 - fuel type: petrol',
+    '4 - color: dark blue, fuel type: diesel, manufacture: Fiat'])
+
+garage([
+    '1 - color: green, fuel type: petrol',
+    '1 - color: dark red, manufacture: WV',
+    '2 - fuel type: diesel',
+    '3 - color: dark blue, fuel type: petrol'])
