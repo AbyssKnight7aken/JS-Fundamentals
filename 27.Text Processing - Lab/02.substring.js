43d36ca3a6e114ad11ebefb4a790f3a8df0626b3
AbyssKnight7aken <7akenbg@gmail.com>
2022-11-09 20:10:23 +0200

    New solved problem!

---
+++ 27.Text Processing - Lab/02.substring.js
@@ -0,0 +1,9 @@
+function substring(string, index, count) {
+
+    let result = string.substring(index, index + count);
+    console.log(result);
+
+}
+
+substring('ASentence', 1, 8)
+substring('SkipWord', 4, 7)
