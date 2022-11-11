5f83dd02401dd8a1532a54c2474e73d8356116d9
Radoslav Petrov <115632451+AbyssKnight7aken@users.noreply.github.com>
2022-11-11 23:24:03 +0200

    New solved problem!

--- 28.Text Processing - Exercise/04.StringSubstring.js
+++ 28.Text Processing - Exercise/04.StringSubstring.js
@@ -1,20 +1,21 @@
-function solve(w,string) {
-	
+function StringSubstring(w, string) {
+
 	const arr = string.split(' ');
 	const word = w.toLowerCase();
-	
-	for(const el of arr){
-		   let curWord = el.toLowerCase();
-		   if(curWord === word){
-		   	console.log(word);
-		   	return;
-		   	}
+
+	for (const el of arr) {
+		let curWord = el.toLowerCase();
+		if (curWord === word) {
+			console.log(word);
+			return;
 		}
-		
-		console.log(`${word} not found!`);
-	
 	}
-	
-solve('javascript','JavaScript is the best programming language')
-solve('python','JavaScript is the bestprogramming language')
+
+	console.log(`${word} not found!`);
+
+}
+
+StringSubstring('javascript', 'JavaScript is the best programming language')
+
+StringSubstring('python', 'JavaScript is the bestprogramming language')
 
