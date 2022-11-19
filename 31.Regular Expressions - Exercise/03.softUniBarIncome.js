8d1f5fd62ddc5c7b1e7bbb13338ed55224c6ef49
AbyssKnight7aken <7akenbg@gmail.com>
2022-11-19 23:25:46 +0200

    changes!

---
+++ 31.Regular Expressions - Exercise/03.softUniBarIncome.js
@@ -0,0 +1,23 @@
+function softUniBarIncome(inputData) {
+
+    const customerInfo = {};
+    let entry = inputData.shift();
+    //let pattern = //g;
+
+    while (entry !== 'end of shift') {
+        //to do...
+    }
+
+}
+
+softUniBarIncome([
+    '%George%<Croissant>|2|10.3$',
+    '%Peter%<Gum>|1|1.3$',
+    '%Maria%<Cola>|1|2.4$',
+    'end of shift'])
+
+softUniBarIncome([
+    '%George%<Croissant>|2|10.3$',
+    '%Peter%<Gum>|1|1.3$',
+    '%Maria%<Cola>|1|2.4$',
+    'end of shift'])
