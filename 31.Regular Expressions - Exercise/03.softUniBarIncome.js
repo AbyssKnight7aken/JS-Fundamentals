20a8c4c7d52d3db52a82d7711b9da85110eaf637
AbyssKnight7aken <7akenbg@gmail.com>
2022-11-21 16:58:03 +0200

    New solved problem!

--- 31.Regular Expressions - Exercise/03.softUniBarIncome.js
+++ 31.Regular Expressions - Exercise/03.softUniBarIncome.js
@@ -1,16 +1,29 @@
 function softUniBarIncome(inputData) {
 
-    const customerInfo = {};
     let entry = inputData.shift();
     let pattern = /%(?<name>[A-Z][a-z]+)%[^|&%.]*<(?<product>\w+)>[^|&%.]*\|(?<count>\d+)\|[^\d|&%.]*(?<price>[0-9|\.]+)\$/g;
+    let totalIncome = 0;
 
     while (entry !== 'end of shift') {
+
         let currentDAta = pattern.exec(entry);
-        console.log(currentDAta);
-        //to do...
+
+        if (currentDAta) {
+            let name = currentDAta.groups.name;
+            let product = currentDAta.groups.product;
+            let count = Number(currentDAta.groups.count);
+            let price = Number(currentDAta.groups.price);
+            let sum = count * price;
+            totalIncome += sum;
+
+            console.log(`${name}: ${product} - ${sum.toFixed(2)}`);
+        }
+
         entry = inputData.shift();
         currentDAta = pattern.exec(entry);
     }
+
+    console.log(`Total income: ${totalIncome.toFixed(2)}`);
 
 }
 
@@ -20,8 +33,3 @@ softUniBarIncome([
     '%Maria%<Cola>|1|2.4$',
     'end of shift'])
 
-softUniBarIncome([
-    '%George%<Croissant>|2|10.3$',
-    '%Peter%<Gum>|1|1.3$',
-    '%Maria%<Cola>|1|2.4$',
-    'end of shift'])
