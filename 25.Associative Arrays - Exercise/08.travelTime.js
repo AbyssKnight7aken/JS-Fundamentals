740cfe5ca338c316ad0accc59c47c62a5f4c6f37
AbyssKnight7aken <7akenbg@gmail.com>
2022-11-29 21:59:44 +0200

    New solved problem!

--- 25.Associative Arrays - Exercise/08.travelTime.js
+++ 25.Associative Arrays - Exercise/08.travelTime.js
@@ -1,64 +1,65 @@
 function travelTime(inputData) {
 
-    let destinations = {};
+    let input = inputData.slice(0);
+    const destinations = {};
+    let entry = input.shift();
 
-    for (let i = 0; i < inputData.length; i++) {
-        let [country, town, cost] = inputData[i].split(` > `);
-        destinations[i] = [country, town, Number(cost)];
-        if (Number(cost) < destinations[i][1]) {
-            destinations[town][1] = Number(cost);
+    while (entry) {
+
+        let [country, town, cost] = entry.split(` > `);
+
+        if (!destinations[country]) {
+            destinations[country] = {};
         }
+
+        if (!destinations[country][town]) {
+            destinations[country][town] = Number(cost);
+        }
+
+        let storedCost = destinations[country][town];
+
+        if (storedCost > Number(cost)) {
+            destinations[country][town] = Number(cost);
+        }
+        entry = input.shift();
     }
 
-    let sorted = Object.entries(destinations).sort((a, b) =>
-        a[1][0].localeCompare(b[1][0]) || (a[1][2] - b[1][2]));
+    let sorted = Object.entries(destinations).sort((a, b) => a[0].localeCompare(b[0]));
+    let result = ''
 
-    for (let k = 0; k < sorted.length; k++) {
-        if (k === sorted.length - 1) { break; }
-        if (sorted[k][1][0] === sorted[k + 1][1][0] && sorted[k][1][1] === sorted[k + 1][1][1]) {
-            sorted[k][1][2] = Math.min(sorted[k][1][2], sorted[k + 1][1][2]);
-            sorted[k+1][1][2] = Math.min(sorted[k][1][2], sorted[k + 1][1][2]);
-            sorted.splice(sorted.indexOf(sorted[k][1][2]) - 2, 1);
-        }
+    for (const element of sorted) {
+        let country = element[0];
+        result += country + " -> ";
+        //console.log(element[0]); //country
+        //console.log(Object.keys(element[1])); //town
+        //console.log(Object.values(element[1])); //cost
+        let sortedTowns = Object.entries(destinations[country]).sort((a, b) => a[1] - b[1]);
+        sortedTowns.forEach(el => {
+            let town = el[0];
+            let cost = el[1];
+            result += `${town} -> ${cost} `;
+        });
+        result += "\n";
     }
 
-    let buffer = [];
-
-    for (let i = 0; i < sorted.length; i++) {
-        let [country, town, cost] = [sorted[i][1][0], sorted[i][1][1], sorted[i][1][2]];
-        if (i === sorted.length - 1) {
-            console.log(`${country} -> ${town} -> ${cost}`); continue;
-        }
-        if ((sorted[i][1][0] === sorted[i + 1][1][0])) {
-            buffer.push(`${town} -> ${cost}`);
-            while (sorted[i][1][0] === sorted[i + 1][1][0]) {
-                i++
-                [country, town, cost] = [sorted[i][1][0], sorted[i][1][1], sorted[i][1][2]];
-                buffer.push(`${town} -> ${cost}`);
-            }
-            console.log(`${country} -> ${buffer.join(` `)}`);
-            buffer = [];
-        } else {
-            console.log(`${country} -> ${town} -> ${cost}`);
-        }
-    }
+    console.log(result);
 
 }
 
-travelTime([
-    "Bulgaria > Sofia > 500",
-    "Bulgaria > Sopot > 800",
-    "France > Paris > 2000",
-    "Albania > Tirana > 1000",
-    "Bulgaria > Sofia > 200"])
+// travelTime([
+//     "Bulgaria > Sofia > 500",
+//     "Bulgaria > Sopot > 800",
+//     "France > Paris > 2000",
+//     "Albania > Tirana > 1000",
+//     "Bulgaria > Sofia > 200"])
 
-travelTime([
-    'Bulgaria > Sofia > 25000',
-    'Bulgaria > Sofia > 25000',
-    'Kalimdor > Orgrimar > 25000',
-    'Albania > Tirana > 25000',
-    'Bulgaria > Varna > 25010',
-    'Bulgaria > Lukovit > 10'])
+// travelTime([
+//     'Bulgaria > Sofia > 25000',
+//     'Bulgaria > Sofia > 25000',
+//     'Kalimdor > Orgrimar > 25000',
+//     'Albania > Tirana > 25000',
+//     'Bulgaria > Varna > 25010',
+//     'Bulgaria > Lukovit > 10'])
 
 travelTime([
     `Bulgaria > Sofia > 25000`,
